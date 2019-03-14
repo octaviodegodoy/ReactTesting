4cb29f87b2607e82ae015cc5f7f3121dfbb135d8
octaviodegodoy <octaviodegodoy@gmail.com>
2019-03-13 22:14:13 -0300

    added HOC

---
+++ build/precache-manifest.88313fb7ce786b018bb9b2f128fc485b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dddd8b1486a1beac6c76",
+    "url": "/static/js/main.13c14039.chunk.js"
+  },
+  {
+    "revision": "940ed37d863d902512a5",
+    "url": "/static/js/2.97a83612.chunk.js"
+  },
+  {
+    "revision": "c74b23304837d696e6f14b69fb5a80c5",
+    "url": "/index.html"
+  }
+];
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,20 @@ import * as actions from 'actions';
 class CommentBox extends Component {
     state = { comment: ''};
 
+    componentDidMount() {
+        this.shouldNavigateAway();
+    }
+
+    componentDidUpdate() {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+        if (!this.props.auth) {
+            console.log("I NEED TO LEAVE !!!");
+        }
+    }
+
     handleChange = event => {
         this.setState({ comment: event.target.value });
     };
@@ -32,4 +46,8 @@ class CommentBox extends Component {
     }
 }
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
